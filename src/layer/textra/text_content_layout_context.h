7ffb62abbe68584776e956e49d5d6be4b9380ffc
Jianliang Wang <wangjianliang0@126.com>
2026-08-21 16:15:10 +0800

    Revert "[Optimize][AnimaX] Fix text color rendering when the document fill co…"
    This reverts commit b92ed1041f1f22cf127c8b47c277956007a4f1c7.

--- src/layer/textra/text_content_layout_context.h
+++ src/layer/textra/text_content_layout_context.h
@@ -5,7 +5,6 @@
 #ifndef ANIMAX_SRC_LAYER_TEXTRA_TEXT_CONTENT_LAYOUT_CONTEXT_H_
 #define ANIMAX_SRC_LAYER_TEXTRA_TEXT_CONTENT_LAYOUT_CONTEXT_H_
 
-#include <cstdint>
 #include <memory>
 #include <vector>
 
@@ -17,7 +16,7 @@ class TextContentDataSource;
 class TextContentLayoutContext {
  public:
   void Layout(const TextContentDataSource& data_source,
-              const skity::Paint& paint, int32_t layer_alpha = 255);
+              const skity::Paint& paint);
   ttoffice::tttext::LayoutRegion* GetLayoutRegion() const {
     return layout_region_.get();
   }
